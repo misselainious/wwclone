1ea2802396c45d64ccd832416ab13e671314bde3
Elaine <elaine.cadman@gmail.com>
2019-10-08 16:13:43 -0700

    pword

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 
 // // Connect to the Atlas
-mongoose.connect( process.env.LOCAL || process.env.ATLAS_URI, { dbName: 'wwcluster', useNewUrlParser: true });
+mongoose.connect( process.env.ATLAS_URI, { useNewUrlParser: true });
 mongoose.connection.on("open", function (ref) {
   console.log("Connected to mongo server.");
 });
